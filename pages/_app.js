f8f3ba14e220a03547d9920a76ecd5208c3e07ba
Michael Corey Munn <coreymunn3@gmail.com>
2021-05-14 10:08:21 -0400

    Added standard head and meta to _app to appear on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
