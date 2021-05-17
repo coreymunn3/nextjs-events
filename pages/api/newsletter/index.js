96c7aa13042d90ea6ffc66f9769a3eb201d05e23
Michael Corey Munn <coreymunn3@gmail.com>
2021-05-16 21:21:37 -0400

    Added MongoDB connection in Newsletter API route, handles signup of email address

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,8 +1,19 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   console.log(req.method);
   if (req.method === 'POST') {
-    console.log(req.body);
-    res.status(201).json({ message: 'success' });
+    const client = await MongoClient.connect(
+      'mongodb+srv://coreymunn:Sherm@n5@cluster0.kdjja.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+    // insert a doc
+    const newDoc = await db
+      .collection('emails')
+      .insertOne({ email: req.body.email });
+    client.close();
+
+    res.status(201).json(newDoc.ops);
   }
 }
 
